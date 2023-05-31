2613425231d029fcc7dd544011b7d66346d34e93
Thabish <kadertabish2@gmail.com>
2023-05-31 12:23:22 +0400

    key
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script>
-	let user = { loggedIn: false };
+	import Thing from '../components/Thing.svelte';
 
-	function toggle() {
-		user.loggedIn = !user.loggedIn;
+	let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' }
+	];
+
+	function handleClick() {
+		things = things.slice(1);
 	}
 </script>
 
-<button on:click={toggle}> Log out </button>
+<button on:click={handleClick}> Remove first thing </button>
 
-<button on:click={toggle}> Log in </button>
+{#each things as thing (thing.id)}
+	<Thing name={thing.name} />
+{/each}
